904f49ce6ade162b2ded99b4c33ff2f10ce25744
FrancescoGentile97 <francesco.gentile97@icloud.com>
2023-02-21 07:43:13 +0100

    Initial commit test

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import TheHeader from "./components/TheHeader.vue";
+export default{
+  components: {TheHeader},
+  data(){
+    return{
+      message:"Bentornato Vue"
+    };
+  },
+};
+</script>
+
+<template>
+  <TheHeader></TheHeader>
+  <h1>{{message}}</h1>
+</template>
+
+<style lang="scss">
+@use "main.scss";
+</style>
+
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,11 @@
+<script>
+
+</script>
+
+<template>
+  <h1>ciao sono un header </h1>
+</template>
+
+<style scoped>
+
+</style>
